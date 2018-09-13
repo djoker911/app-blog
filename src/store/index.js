f44999524e3d2f5041c1bd1584c5a4fb221f6181
Phil <philwu@systex.com>
2018-09-13 09:49:56 +0800

    Vuex example for basic function

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import user from './user.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        count: 0
+        count: 0,
+        name: "Phil"
     },
     mutations: {
         setCountAdd (state) {
@@ -13,6 +15,12 @@ export default new Vuex.Store({
         },
         setCountMinus (state) {
             state.count --;
+        },
+        setCountAdd10 (state, payload) {
+            state.count += payload ;
+        },
+        setCountMinus10 (state, payload) {
+            state.count = state.count - payload;
         }
     },
     actions: {
@@ -21,12 +29,21 @@ export default new Vuex.Store({
         },
         minus1({commit}) {
             commit('setCountMinus')
+        },
+        add10({commit}, payload) {
+            commit('setCountAdd10', payload)
+        },
+        minus10({commit}, payload) {
+            commit('setCountMinus10', payload)
         }
     },
     getters: {
-
+        getCountClick (state) {
+            return state.count > 10 ? "big" : "small"
+            //return state.count + "click(s)"
+        }
     },
     modules: {
-
+        user
     }
 })
