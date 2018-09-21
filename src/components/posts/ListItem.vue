4786e363315f8065fd70d6275fd48f052d5cb206
Phil <philwu@systex.com>
2018-09-21 11:05:35 +0800

    multi select using checkbox

--- src/components/posts/ListItem.vue
+++ src/components/posts/ListItem.vue
@@ -1,16 +1,23 @@
 <template>
-    <a class="weeeee" @click="onClicked">
+    <!--<a class="weeeee">
         <div>
-            {{post.id}},{{post.domainName}}, {{timeStamp}}
+            {{post.id}},{{post.domainName}}, {{timeStamp}}, <button name="subject" type="submit" value="fav_HTML" @click="onClicked">EDIT</button>
         </div>
-    </a>
-    
+    </a>-->
+    <div class="divTableRow">
+        <div class="divTableCell"><input type="checkbox" :id="post.id" :value="post.id" @change="onChange($event.target)"></div>
+        <div class="divTableCell">{{post.id}}</div>
+        <div class="divTableCell">{{post.domainName}}</div>
+        <div class="divTableCell">{{timeStamp}}</div>
+        <div class="divTableCell"><button name="subject" type="submit" value="fav_HTML" @click="onClicked">EDIT</button></div>
+    </div>
     
 </template>
 <script>
 import moment from 'moment'
 
 export default {
+    
     props: ['post'],
     computed: {
         timeStamp() {
@@ -20,6 +27,11 @@ export default {
     methods: {
         onClicked () {
             this.$emit('onClickedItem', this.post.id)
+        },
+        onChange(val) {
+            console.log(val.id);
+            console.log(val.checked);
+            this.$emit("change", val.id, val.checked)
         }
     }
 }    
--- src/components/posts/PostsList.vue
+++ src/components/posts/PostsList.vue
@@ -1,12 +1,32 @@
 <template lang="html">
     <div class="container">
         <h1>PostsList</h1>
-       <div class='list-group'>
+        <div class="control-btn">
+            <button name="add">ADD</button>
+            <button name="delete">DELETE</button>
+            <button name="copy">COPY</button>
+        </div>
+         <div class="divTable">
+        <div class="divTableBody">
+        <div class="divTableRow">
+        <div class="divTableCell" style="width:5%">&nbsp;</div>
+        <div class="divTableCell" style="width:10%">id</div>
+        <div class="divTableCell">&nbsp;domain</div>
+        <div class="divTableCell" style="width:20%">&nbsp;create_time</div>
+        <div class="divTableCell">&nbsp</div>
+        </div>
+        <list-item v-for="post in $store.state.post.posts" :post="post" :key="post.id"
+            @change="checkedItem" @onClickedItem="updateItem"></list-item>
+        </div>
+       <!--<div class='list-group'>
             <list-item v-for="post in $store.state.post.posts" :post="post" :key="post.id"
             @onClickedItem="updateItem"></list-item>
         </div>
-        <router-view></router-view>
-    </div>    
+        <router-view></router-view>-->   
+    </div>
+        <p>Checked Ids: {{ checkboxValue }}</p>
+    </div> 
+       
 </template>
 
 <script>
@@ -14,6 +34,12 @@
 
    import ListItem from './ListItem.vue'
    export default{
+       data() {
+           return {
+               checkboxValue:[],
+               checkedVals: ['bar']
+           }
+       },
        created () {
         this.$store.dispatch('getPosts', this.user.group)
        },
@@ -29,6 +55,17 @@
         methods :{
             updateItem (payload) {
                 console.log("updating post "+ payload)
+            },
+            checkedItem(value, checked) {
+                console.log("checked "+checked)
+                console.log("checked value "+value)
+                if (checked) {
+                    console.log("checked is true")
+                    this.checkboxValue.push(value)
+                } else {
+                    console.log('index is '+this.checkboxValue.indexOf(value))
+                    this.checkboxValue.splice(this.checkboxValue.indexOf(value), 1)
+                }
             }
         }
 
@@ -39,5 +76,34 @@
 </script>
 
 <style lang="css">
-
+/* DivTable.com */
+.divTable{
+	display: table;
+	width: 100%;
+}
+.divTableRow {
+	display: table-row;
+}
+.divTableHeading {
+	background-color: #EEE;
+	display: table-header-group;
+}
+.divTableCell, .divTableHead {
+	border: 1px solid #999999;
+	display: table-cell;
+	padding: 3px 10px;
+}
+.divTableHeading {
+	background-color: #EEE;
+	display: table-header-group;
+	font-weight: bold;
+}
+.divTableFoot {
+	background-color: #EEE;
+	display: table-footer-group;
+	font-weight: bold;
+}
+.divTableBody {
+	display: table-row-group;
+}
 </style>
